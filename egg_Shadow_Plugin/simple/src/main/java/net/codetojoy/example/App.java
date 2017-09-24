c09e0f55bed8c1437bd5592c2624b661285b4936
Michael Easter <codetojoy@gmail.com>
2017-09-24 07:36:14 -0300

    experimenting with Shadow plugin for executable jar

---
+++ egg_Shadow_Plugin/simple/src/main/java/net/codetojoy/example/App.java
@@ -0,0 +1,11 @@
+
+package net.codetojoy.example;
+
+import org.joda.time.DateTime;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("TRACER time: " + new DateTime());
+        System.out.println("Ready.");
+    }
+}

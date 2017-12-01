8a4b4cc639fce8e69b290c956f1d4590b81247d5
Michael Easter <codetojoy@gmail.com>
2017-12-01 09:50:29 -0400

    Stack Overflow example

---
+++ egg_StackOverflow_47579159/README.md
@@ -0,0 +1,2 @@
+
+* example for [this question](https://stackoverflow.com/questions/47579159) on Stack Overflow
---
+++ egg_StackOverflow_47579159/src/main/java/net/codetojoy/Example.java
@@ -0,0 +1,9 @@
+
+package net.codetojoy;
+
+import org.postgresql.core.Query;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Example {
+}

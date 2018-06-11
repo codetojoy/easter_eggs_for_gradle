5ee8a691012d1c7d161eb8bcb14aeb1fa6ba6fe6
Michael Easter <codetojoy@gmail.com>
2018-06-11 09:57:43 -0300

    incremental change as part of a larger frenzy of activity

---
+++ egg_Signing/resources/TemplateBuildInfo.java
@@ -0,0 +1,20 @@
+
+package __PACKAGE;
+
+public class BuildInfo {
+    private static final String artifact = "__ARTIFACT";
+    private static final String version = "__VERSION";
+    private static final String buildTimestamp = "__BUILD_TIMESTAMP";
+    private static final String gitCommitInfo = "__GIT_COMMIT_INFO";
+
+    public String toString() {
+        StringBuilder buffer = new StringBuilder();
+
+        buffer.append("\nartifact      : " + artifact + "\n");
+        buffer.append("version         : " + version + "\n");
+        buffer.append("build timestamp : " + buildTimestamp + "\n");
+        buffer.append("git info        : " + gitCommitInfo + "\n");
+
+        return buffer.toString();
+    }
+}

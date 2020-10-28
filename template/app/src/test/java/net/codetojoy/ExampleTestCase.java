f3843f46e0071bff92921cdc6b6a120dfa913af1
Michael Easter <codetojoy@gmail.com>
2020-10-28 13:15:01 -0300

    new template

---
+++ template/app/src/test/java/net/codetojoy/ExampleTestCase.java
@@ -0,0 +1,39 @@
+
+package net.codetojoy;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.*;
+import java.util.stream.*;
+
+class Location {
+    String type;
+    public String getType() { return type; }
+}
+
+class LocationResponse {
+    List<Location> locations = new ArrayList<>();
+    public List<Location> getLocations() { return locations; }
+}
+
+public class ExampleTestCase {
+    @Test
+    public void testCanary() {
+        int x = 2+2;
+        assertEquals(4, x);
+    }
+
+    @Test
+    public void testExample() {
+        var loc = new Location();
+        var list = List.of(loc, new Location(), new Location());
+        var locationResponse = new LocationResponse();
+        locationResponse.locations.addAll(list);
+
+        locationResponse.getLocations()
+                        .stream()
+                        .map(location -> location.getType())
+                        .forEach(t -> assertNull(t));
+    }
+}

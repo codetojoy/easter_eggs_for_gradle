f5637d922e6907b3a4f89755b0e0b478f2c0968c
Michael Easter <codetojoy@gmail.com>
2020-11-02 18:42:30 -0400

    incremental update Mon  2 Nov 2020 18:42:30 AST

--- egg_StackOverflow_64652901/go.sh
+++ egg_StackOverflow_64652901/go.sh
@@ -4,8 +4,14 @@ set -e
 
 cp ./Original.txt src/main/java/Example.java
 
+echo "-------------------------------"
+echo "ORIGINAL file:"
 cat src/main/java/Example.java
+echo "-------------------------------"
 
 gradle fmt 
 
+echo "-------------------------------"
+echo "FORMATTED file:"
 cat src/main/java/Example.java
+echo "-------------------------------"

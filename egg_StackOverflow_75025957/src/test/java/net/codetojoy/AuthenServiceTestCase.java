95b81e6cd3fb0aa8199ba45963a1908eb3e163bf
Michael Easter <codetojoy@gmail.com>
2023-01-06 10:59:13 -0400

    updated for JUnit 5

--- egg_StackOverflow_75025957/src/test/java/net/codetojoy/AuthenServiceTestCase.java
+++ egg_StackOverflow_75025957/src/test/java/net/codetojoy/AuthenServiceTestCase.java
@@ -1,30 +1,27 @@
 
 package net.codetojoy;
 
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 import static org.mockito.Mockito.*;
-
-import java.util.*;
-import java.util.stream.*;
 
 public class AuthenServiceTestCase {
     AuthenService as;
     UserRepo ur;
 
-    @Before
+    @BeforeEach
     public void init() {
         ur = mock(UserRepo.class);
         as = new AuthenService(ur);
     }
 
-    @Test(expected = UserNotFoundException.class)
+    @Test
     public void authenticate() throws UserNotFoundException {
         when(ur.validate("abc", "123")).thenReturn(false);
 
         // test
-        as.authenticate("abc", "123");
+        assertThrows(UserNotFoundException.class, () ->as.authenticate("abc", "123"));
     }
 }
 
